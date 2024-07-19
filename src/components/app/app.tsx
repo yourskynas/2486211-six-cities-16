2d62a922ca350cbbe0f07a9951740a5d4f0f7883
yourskynas <nastiazaharova69@gmail.com>
2024-07-19 18:19:47 +0300

    Добавит компонент для страницы «404»

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,11 @@
-import { PlaceOfferType } from '../../types';
+import { OfferType, PlaceOfferType } from '../../types';
 import MainPage from '../../pages/main-page';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AppRoute } from '../../constants';
 import LoginPage from '../../pages/login-page';
 import OfferPage from '../../pages/offer-page';
 import FavoritesPage from '../../pages/favorites-page';
+import NotFoundPage from '../../pages/not-found-page';
 
 type PlaceOffersProps = PlaceOfferType[];
 
@@ -13,9 +14,10 @@ type AppProps = {
   cities: string[];
   placesOptions: string[];
   placeOffers: PlaceOffersProps;
+  offer: OfferType;
 }
 
-const App = ({countOffers, cities, placesOptions, placeOffers}: AppProps): JSX.Element => (
+const App = ({countOffers, cities, placesOptions, placeOffers, offer}: AppProps): JSX.Element => (
   <BrowserRouter>
     <Routes>
       <Route
@@ -35,11 +37,15 @@ const App = ({countOffers, cities, placesOptions, placeOffers}: AppProps): JSX.E
       />
       <Route
         path={AppRoute.OFFER}
-        element={<OfferPage />}
+        element={<OfferPage offer={offer}/>}
       />
       <Route
         path={AppRoute.FAVORITES}
         element={<FavoritesPage placeOffers={placeOffers} />}
+      />
+      <Route
+        path="*"
+        element={<NotFoundPage />}
       />
     </Routes>
   </BrowserRouter>
---
+++ src/pages/not-found-page.tsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+import Logo from '../components/logo/logo';
+
+const NotFoundPage = (): JSX.Element => (
+  <div className="page page--gray page--login">
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__left">
+            <Logo classNameLogo={''} imageWidth={''} imageHeight={''}/>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <main className="page__main page__main--login">
+      <div className="page__login-container container">
+        <section className="login">
+          <h1 className="login__title">404 | Страница не найдена</h1>
+          <div className="login__form form">
+            <Link to='/' className="login__submit form__submit button">Перейти на главную</Link>
+          </div>
+        </section>
+        <section className="locations locations--login locations--current">
+          <div className="locations__item">
+            <Link className="locations__item-link" to="/">
+              <span>Amsterdam</span>
+            </Link>
+          </div>
+        </section>
+      </div>
+    </main>
+  </div>
+);
+
+export default NotFoundPage;
