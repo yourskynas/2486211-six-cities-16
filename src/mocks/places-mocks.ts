df10f26b67cc81c3478f999e8a2937e322d921b8
yourskynas <nastiazaharova69@gmail.com>
2024-07-19 04:56:27 +0300

    Создает компонент-страницу favorites-page.tsx

--- src/mocks/places-mocks.ts
+++ src/mocks/places-mocks.ts
@@ -18,7 +18,7 @@ const placeOffers = [
       longitude: 4.883976,
       zoom: 16
     },
-    isFavorite: true,
+    isFavorite: false,
     isPremium: false,
     rating: 4.2
   },
@@ -41,7 +41,7 @@ const placeOffers = [
       longitude: 4.883976,
       zoom: 16
     },
-    isFavorite: false,
+    isFavorite: true,
     isPremium: true,
     rating: 3.1
   },
@@ -64,7 +64,7 @@ const placeOffers = [
       longitude: 4.9179759999999995,
       zoom: 16
     },
-    isFavorite: false,
+    isFavorite: true,
     isPremium: true,
     rating: 4.7
   },
@@ -110,9 +110,101 @@ const placeOffers = [
       longitude: 4.887976,
       zoom: 16
     },
-    isFavorite: false,
+    isFavorite: true,
     isPremium: false,
     rating: 1.4
+  },
+  {
+    id: '4ca1ea41-8f8b-471b-8246-87745ec252d1',
+    title: 'Beautiful & luxurious apartment at great location',
+    type: 'house',
+    price: 498,
+    previewImage: 'https://16.design.htmlacademy.pro/static/hotel/7.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 52.37454,
+        longitude: 4.897976,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 52.36854,
+      longitude: 4.887976,
+      zoom: 16
+    },
+    isFavorite: true,
+    isPremium: false,
+    rating: 1.4
+  },
+  {
+    id: 'cb2c441d-58c2-41b5-8168-9b564214dfab',
+    title: 'Penthouse, 4-5 rooms + 5 balconies',
+    type: 'house',
+    price: 387,
+    previewImage: 'https://16.design.htmlacademy.pro/static/hotel/20.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 48.834610000000005,
+      longitude: 2.335499,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: true,
+    rating: 2
+  },
+  {
+    id: '2b1d1f4b-990c-4e1a-abe5-d0405da10fff',
+    title: 'Loft Studio in the Central Area',
+    type: 'house',
+    price: 390,
+    previewImage: 'https://16.design.htmlacademy.pro/static/hotel/9.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 48.868610000000004,
+      longitude: 2.342499,
+      zoom: 16
+    },
+    isFavorite: true,
+    isPremium: true,
+    rating: 3.4
+  },
+  {
+    id: '36b7a660-a8a4-4a4d-b3e8-732ca64c8cd4',
+    title: 'Beautiful & luxurious apartment at great location',
+    type: 'room',
+    price: 273,
+    previewImage: 'https://16.design.htmlacademy.pro/static/hotel/12.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 48.858610000000006,
+      longitude: 2.330499,
+      zoom: 16
+    },
+    isFavorite: true,
+    isPremium: false,
+    rating: 1.7
   }
 ];
 
