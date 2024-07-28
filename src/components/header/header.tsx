deef4476268f48fcc852f38ce352450fb738c9fe
yourskynas <nastiazaharova69@gmail.com>
2024-07-28 21:49:39 +0300

    Вносит мелкие правки

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,12 +9,12 @@ type HeaderProps = {
 const NavForUser = (): JSX.Element => (
   <>
     <li className="header__nav-item user">
-      <a className="header__nav-link header__nav-link--profile" href="#">
+      <Link className="header__nav-link header__nav-link--profile" to={AppRoute.FAVORITES}>
         <div className="header__avatar-wrapper user__avatar-wrapper">
         </div>
         <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
         <span className="header__favorite-count">3</span>
-      </a>
+      </Link>
     </li>
     <li className="header__nav-item">
       <a className="header__nav-link" href="#">
