71fe7d03ac82c6908a362a9d2eb929862a3ee762
Anastasia Zakharova <nastiazaharova69@gmail.com>
2024-08-13 14:50:34 +0300

    Конфигурирует стор, оборачивает App в пакет для связи реакта с редаксом

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import { AuthorizationStatus, CITIES, PLACES_OPTIONS } from './constants';
 import { placeOffers } from './mocks/places-mocks';
 import { offer } from './mocks/offer-mocks';
 import { reviews } from './mocks/reviews-mocks';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,13 +14,15 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      cities = {CITIES}
-      placesOptions = {PLACES_OPTIONS}
-      placeOffers = {placeOffers}
-      offer = {offer}
-      reviews={reviews}
-      authorizationStatus={AuthorizationStatus.AUTH}
-    />
+    <Provider store={store}>
+      <App
+        cities = {CITIES}
+        placesOptions = {PLACES_OPTIONS}
+        placeOffers = {placeOffers}
+        offer = {offer}
+        reviews={reviews}
+        authorizationStatus={AuthorizationStatus.AUTH}
+      />
+    </Provider>
   </React.StrictMode>
 );
