7e91d109e7e88b35cfb606026ef3021efbb3161c
Anastasia Zakharova <nastiazaharova69@gmail.com>
2024-08-14 00:34:45 +0300

    Выносит функцию для группировки по городам в утилки

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { CityName, PlaceOfferType } from './types';
+
 const ratingInProcent = (rating: number): string => {
   const procent = 100;
   const ratingScale = 5;
@@ -13,4 +15,16 @@ const humanizingDate = (value: string) => {
   return formatedDate;
 };
 
-export { ratingInProcent, humanizingDate };
+const groupByCity = (offers: PlaceOfferType[], isFavoritePage = false) => offers.reduce((group: Record<CityName, PlaceOfferType[]>, offer: PlaceOfferType) => {
+  const city = offer.city.name;
+  group[city] = group[city] ?? [];
+  if (isFavoritePage && offer && offer.isFavorite) {
+    group[city].push(offer);
+  } else if (!isFavoritePage) {
+    group[city].push(offer);
+  }
+  // group[city].push(offer);
+  return group;
+}, {});
+
+export { ratingInProcent, humanizingDate, groupByCity };
