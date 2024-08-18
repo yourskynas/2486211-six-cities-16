97ac56748c7b80a4b2e93df1241918029b2a0b6a
Anastasia Zakharova <nastiazaharova69@gmail.com>
2024-08-19 01:48:21 +0300

    Сделает компонент для отображения ошибки и подключит?

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-import { AuthorizationStatus, CITIES } from './constants';
+import { CITIES } from './constants';
 import { offer } from './mocks/offer-mocks';
 import { reviews } from './mocks/reviews-mocks';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { fetchFavoritesOffersAction, fetchOffersAction } from './store/api-actions';
+import { checkAuthAction, fetchFavoritesOffersAction, fetchOffersAction } from './store/api-actions';
+import Error from './components/empty-stubs/error';
 
 store.dispatch(fetchOffersAction());
 store.dispatch(fetchFavoritesOffersAction());
+store.dispatch(checkAuthAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -18,11 +20,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      {/* всё падает, если раскомментировать <Error /> */}
       <App
         cities = {CITIES}
         offer = {offer}
         reviews={reviews}
-        authorizationStatus={AuthorizationStatus.AUTH}
       />
     </Provider>
   </React.StrictMode>
