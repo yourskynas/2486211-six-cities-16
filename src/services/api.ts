454c72bf6d7e1b8fa89fa21217c531ca11967330
Anastasia Zakharova <nastiazaharova69@gmail.com>
2024-08-19 01:51:32 +0300

    Добавит модуль process-error-handle.ts обновления ошибки и обработку ошибок взаимодействия с сервером

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,23 @@
-import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
+import axios, { AxiosError, AxiosInstance, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 import { getToken } from './token';
+import { StatusCodes } from 'http-status-codes';
+import { processErrorHandle } from './process-error-handle';
 
 const BACKEND_URL = 'https://16.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
+
+type DetailMessageType = {
+  type: string;
+  message: string;
+}
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: true,
+  [StatusCodes.NOT_FOUND]: true
+};
+
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
 
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
@@ -20,6 +35,19 @@ export const createAPI = (): AxiosInstance => {
 
       return config;
     },
+  );
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<DetailMessageType>) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        const detailMessage = (error.response.data);
+
+        processErrorHandle(detailMessage.message);
+      }
+
+      throw error;
+    }
   );
 
   return api;
